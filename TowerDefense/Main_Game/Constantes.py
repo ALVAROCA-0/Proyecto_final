c530f87c35a833beaa68a064358181d656a493dd
NicolasPajaro <pajarosancheznicolas@gmail.com>
2024-09-27 21:48:48 -0500

    Update Constantes.py

--- TowerDefense/Main_Game/Constantes.py
+++ TowerDefense/Main_Game/Constantes.py
@@ -1,4 +1,25 @@
-VENTANA_ALTURA = 500
-VENTANA_ANCHO = 500
+FILAS = 20
+COLUMNAS = 20
+TAMAÑO_PIXEL = 32
+vertices = [
+    (560,0),
+    (560,112),
+    (240,112),
+    (240,48),
+    (80,48),
+    (80,336),
+    (176,336),
+    (176,272),
+    (336,272),
+    (336,304),
+    (592,304),
+    (592,560),
+    (432,560),
+    (432,432),
+    (272,432),
+    (272,560),
+    (0,560)
+]
+VENTANA_ALTURA = FILAS*TAMAÑO_PIXEL
+VENTANA_ANCHO = COLUMNAS*TAMAÑO_PIXEL
 FPS = 60
-
